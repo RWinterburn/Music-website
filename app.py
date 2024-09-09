9a2a12569a52407ef9ddb55215547ae72bb3ae9e
Rob Winterburn <forestalwinters@gmail.com>
2024-09-09 12:15:09 +0000

    adding to website

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+from models import software
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = software()
+    return render_template('index.html', **data)
+
+@app.route('/home')
+def home():
+    data = software()
+    return render_template('home.html', **data)
+
+@app.route('/music')
+def music():
+    data = software()
+    return render_template('music.html', **data)
+
+@app.route('/videos')
+def videos():
+    data = software()
+    return render_template('videos.html', **data)
+
+@app.route('/merch')
+def merch():
+    data = software()
+    return render_template('merch.html', **data)
+
+
+@app.route('/shows')
+def shows():
+    data = software()
+    return render_template ('shows.html', **data)
+
+@app.route('/gallery')
+def gallery():
+    data = software()
+    return render_template ('gallery.html', **data)
+
+@app.route('/contact')
+def contact():
+    data = software()
+    return render_template ('contact.html', **data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.gallery-container img');
+
+    images.forEach(img => {
+        img.addEventListener('click', function() {
+            // Remove 'enlarged' class from any image that has it
+            images.forEach(image => image.classList.remove('enlarged'));
+
+            // Add 'enlarged' class to the clicked image
+            this.classList.add('enlarged');
+        });
+    });
+
+    // Optionally, add a click event listener on the body to remove 'enlarged' class when clicking outside the image
+    document.body.addEventListener('click', function(event) {
+        if (!event.target.matches('.gallery-container img')) {
+            images.forEach(img => img.classList.remove('enlarged'));
+        }
+    });
+});
